16deb8eac86e265dc1c88ddc00d2dc3561ba09e6
drem-jpg <drem687sawesomeemail@gmail.com>
2025-06-23 02:16:42 -0400

    lots of stuff

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=10"
+	"size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,9 +14,9 @@ static const char *colors[SchemeLast][2] = {
 //	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
 //	[SchemeOut] = { "#000000", "#00ffff" },
 	
-	[SchemeNorm] = { "#f1be9b", "#020914" },
-	[SchemeSel] = { "#f1be9b", "#565879"},
-	[SchemeOut] = { "#f1be9b", "#DA6748" },
+	[SchemeNorm] = { "#eeeeee", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#61afef"},
+	[SchemeOut] = { "#eeeeee", "#61afef" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
--- slstatus/config.h
+++ slstatus/config.h
@@ -65,5 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",  "%a %d %b %Y, %r"},
+  { ram_used, " 󰘚 %s ",  NULL },
+	{ datetime, "󰥔 %s",  "%a %d %b %Y, %r"},
 };
