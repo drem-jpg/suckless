cf5ce65a71b59bb4d63d2b30c4a273f8a8e01437
drem-jpg <drem687sawesomeemail@gmail.com>
2025-06-25 11:53:18 -0400

    remove slock and dwmblocks as I don't use them

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,20 +4,25 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"size=12"
+	"monospace:size=10",
+	"NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
 };
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	//Nord 
-//	[SchemeNorm] = { "#E5E9F0", "#3B4252" },
-//	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-	
-	[SchemeNorm] = { "#eeeeee", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#61afef"},
-	[SchemeOut] = { "#eeeeee", "#61afef" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
 static unsigned int columns    = 1;
@@ -30,3 +35,4 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static const unsigned int border_width = 0;
+
