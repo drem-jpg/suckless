e99036054c1102d75eeac2623ca69c555690c712
drem-jpg <drem687sawesomeemail@gmail.com>
2025-06-29 13:47:45 -0400

    patch dwm further; removed some patches because of issues. added dwmblocks

---
+++ dwm/pywal.h
@@ -0,0 +1,6 @@
+static const char norm_bg[] = "#10100a";
+static const char norm_fg[] = "#c2c1aa";
+static const char norm_border[] = "#10100a";
+static const char sel_bg[] = "#3C584C";
+static const char sel_fg[] = "#10100a";
+static const char sel_border[] = "#858862";
