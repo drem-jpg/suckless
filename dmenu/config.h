61684c47b6bbef08244d8578a9d33746da299615
drem-jpg <drem687sawesomeemail@gmail.com>
2025-06-30 02:02:48 -0400

    update dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -7,17 +7,27 @@ static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mono:size=15"
+	"monospace:size=15",
+	"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"
 };
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#efefef", "#1c1c1c" },
-	[SchemeSel] = { "#efefef", "#225d44" },
-	[SchemeOut] = { "#000000", "#2c2c2c" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 20;
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
